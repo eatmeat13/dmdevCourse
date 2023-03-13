696d7b9a2125dc8ff3769b31bc4094cbf3b0711a
lmyasoedov <myasoedov.leon@mail.ru>
2023-03-13 23:02:33 +0300

    all

--- src/sixthhomework/ThirdTask.java
+++ src/sixthhomework/ThirdTask.java
@@ -23,7 +23,7 @@ import static java.util.stream.Collectors.toList;
 public class ThirdTask {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
-        students.add(new Student("Leonid", "Myasoedov", 4, List.of(2, 2, 2,2)));
+        students.add(new Student("Leonid", "Myasoedov", 4, List.of(2, 2, 2, 2)));
         students.add(new Student("Ivan", "Ivanov", 3, List.of(2, 3, 4, 5, 2)));
         students.add(new Student("Vasya", "Smirnov", 2, List.of(3, 3, 3, 3, 3)));
         students.add(new Student("Vika", "Ulyanova", 5, List.of(4, 3, 4, 2, 2)));
